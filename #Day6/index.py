4f154bc8d98a4a9e37de8a844eb3f8b9a1b790df
wahi90 <mdshahed1098@gmail.com>
2022-12-03 21:03:12 +0600

    day6 finished

---
+++ #Day6/index.py
@@ -0,0 +1,21 @@
+n = 232321233
+name = "Sahed"
+print(n)
+print(name)
+b = True
+d = None
+# print(type(n))
+# print(type(name))
+# print(type(b))
+# print(type(d))
+
+num = complex(8,8)
+
+print(num)
+# list is list of items that has any data types
+list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+print(list1)
+tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+print(tuple1)
+dict1 = {"name":"Sakshi", "age":20, "canVote":True}
+print(dict1)
