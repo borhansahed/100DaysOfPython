5667f75355a806bdd99b3eae32bc09fd6eda6e88
wahi90 <mdshahed1098@gmail.com>
2022-12-08 20:58:40 +0600

    finished 11day

---
+++ #day11/index.py
@@ -0,0 +1,23 @@
+name = "sahed"
+friend = "mehedi"
+anotherFriend = "sajjad"
+
+print(name +" "+ friend)
+
+# we can use three '''      ''' for string when you have to write multiple line in one string
+
+str = ''' sahed 
+dadfa
+dfa
+fad
+fa
+df
+'''
+
+print(str)
+# in python string used array indexing system to access string character
+print(name[0])
+
+# we can iterate full string character by a for loop
+for c in name:
+ print(c)
