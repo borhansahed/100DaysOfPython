046a88f3a3959b1c6c5ceb68aa5deff7bbecaed3
wahi90 <mdshahed1098@gmail.com>
2022-12-09 21:31:09 +0600

    #day12 done

---
+++ #day12/index.py
@@ -0,0 +1,12 @@
+name = "Hello sahed"
+
+print(name[-1]) # you will get last element of string
+
+print(len(name)) # you will get string length 
+print((name[0:5])) # you will get index 0 to index 4
+print(len(name[0:5])) # you will get length of slice string
+nam = "Harry"
+print(nam[-4:-2])
+
+# for i in name:
+#     print(i)
