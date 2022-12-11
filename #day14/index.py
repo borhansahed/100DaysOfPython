0f9cfe73d8670fc8edd10f0264abe78ce9a7d4b5
wahi90 <mdshahed1098@gmail.com>
2022-12-11 21:33:01 +0600

    day14 completed

---
+++ #day14/index.py
@@ -0,0 +1,17 @@
+
+age = int(input("Enter your age: "))
+
+print("your age is", age)
+
+# conditional operators
+# >, < , <=, >=, ==, !=
+# in python && == and ,  || == or
+
+if (age > 30):
+    print("you should marry now")
+elif(age == 21):
+    print("you are available for marry")
+elif(age < 18 and age > 12):
+    print("please donot think about others")
+else:
+    print("You have to earn money")
