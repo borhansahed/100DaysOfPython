57dd23017446d5eac36c6a2f46488d813d10a3ac
wahi90 <mdshahed1098@gmail.com>
2022-12-15 22:08:26 +0600

    done 18day

---
+++ #Day18/index.py
@@ -0,0 +1,16 @@
+# you can else with while loop
+
+i = 1 
+# while(i<=4):
+#     print(i)
+#     i = i + 1
+# else:
+#     print("i is Greater than 4")
+
+
+while(True):
+    print(i)
+    i +=1
+    if i > 10:
+        break
+
